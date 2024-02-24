9839cf1bee318213bdc666a01314b71c3a93de9c
Anirooddh patidar <135500727+ANIROODDH@users.noreply.github.com>
2024-02-24 19:31:03 +0530

    "first commit"

---
+++ my-app/src/components/Footer.js
@@ -0,0 +1,71 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <div>
+      <div className="footer-top">
+        <div className="container cnt">
+          <div className="row rw">
+            <div className="col-lg-4 col-md-6 footer-contact">
+              <h3>QuanTechL2M</h3>
+              <p>
+                401 Shikharji Apartment, Rau Rajendra Nagar, Indore, Madhya Pradesh, India <br /><br />
+                <strong>Phone:</strong> +1 5589 55488 55<br />
+                <strong>Email:</strong> QuanTechL2M@gmail.com<br />
+              </p>
+
+              <div className="social-links mt-3">
+                <a href="https://www.google.com/url?q=https%3A%2F%2Fx.com%2FQuanTechL2M%3Ft%3Dd0GCwp8Ul8rwCjBcU_c8lw%26s%3D08&sa=D&sntz=1&usg=AOvVaw3_i552karmYHo3Xnk_deyz" className="twitter"><i className="bx bxl-twitter"></i></a>
+                <a href="https://www.google.com/url?q=https%3A%2F%2Fwww.facebook.com%2Fprofile.php%3Fid%3D61554442277389&sa=D&sntz=1&usg=AOvVaw23OZDx6nCzglNWLN_X7jL3" className="facebook"><i className="bx bxl-facebook"></i></a>
+                <a href="https://www.instagram.com/quantechl2m" className="instagram"><i className="bx bxl-instagram"></i></a>
+                <a href="https://www.youtube.com/@QuanTechL2M" className="youtube"><i className="bx bxl-youtube"></i></a>
+                <a href="https://github.com/quantechl2m" className="github"><i className="bx bxl-github"></i></a>
+              </div>
+            </div>
+
+            <div className="col-lg-2 col-md-6 footer-links">
+              <h4>Our Company</h4>
+              <ul>
+                <li><i className="bx bx-chevron-right"></i> <a href="about.html">About Us</a></li>
+                <li><i className="bx bx-chevron-right"></i> <a href="about.html">Members</a></li>
+                <li><i className="bx bx-chevron-right"></i> <a href="awards.html">Awards</a></li>
+                <li><i className="bx bx-chevron-right"></i> <a href="contact.html">Terms & Condition</a></li>
+                <li><i className="bx bx-chevron-right"></i> <a href="contact.html">Privacy policy</a></li>
+              </ul>
+            </div>
+
+            <div className="col-lg-2 col-md-6 footer-links">
+              <h4>Our Products</h4>
+              <ul>
+                <li><i className="bx bx-chevron-right"></i> <a href="gas-sensor.html">Gas Sensor</a></li>
+                <li><i className="bx bx-chevron-right"></i> <a href="bio-sensor.html">Bio Sensor</a></li>
+                <li><i className="bx bx-chevron-right"></i> <a href="agricultural-sensor.html">Agricultural Sensor</a></li>
+              </ul>
+            </div>
+
+            <div className="col-lg-2 col-md-6 footer-links">
+              <h4>Support</h4>
+              <ul>
+                <li><i className="bx bx-chevron-right"></i> <a href="contact.html">FAQ</a></li>
+                <li><i className="bx bx-chevron-right"></i> <a href="contact.html">Contact</a></li>
+                <li><i className="bx bx-chevron-right"></i> <a href="career.html">Career Opportunity</a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="container footer-bottom clearfix">
+        <div className="copyright">
+          &copy; <strong><span>2023 QuanTechL2M Innovations Private Limited.</span></strong>All Rights Reserved
+        </div>
+        <div className="credits">
+          <a href="index.html">Home |</a>
+          <a href="career.html">Career |</a>
+          <a href="about.html">About us |</a>
+          <a href="contact.html">Contact </a>
+        </div>
+      </div>
+    </div>
+  )
+}
